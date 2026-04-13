a6ba74f96cd6aa46c12845a4d87df52304414fcd
chiscookeke11 <chiscookeke11@gmail.com>
2026-04-13 06:56:00 +0100

    setup congratulations screen

--- app/auth/screens/CongratulationsScreen.tsx
+++ app/auth/screens/CongratulationsScreen.tsx
@@ -1,11 +1,15 @@
+import { AuthStackParamList } from '@/app/auth/types';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { StyleSheet, Text } from "react-native";
 import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 
 
-
-
 export default function CongratulationsSection() {
+    const navigation = useNavigation<NativeStackNavigationProp<AuthStackParamList>>();
+
+
     return (
         <View style={styles.container} >
 
